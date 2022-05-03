81162a7df353af7fd4ea386a390925745dd7137b
temptado <jason1287712@gmail.com>
2022-05-02 21:37:31 -0400

    set up grid template areas for weather grid

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 const Article = () => {
     return (
-        <article>
+        <article className="weather-article">
             <h2>Date</h2>
             <img src="" alt="" />
             <p>temp</p>
             <p>wind</p>
             <p>humidity</p>
-
         </article>
     )
 }
