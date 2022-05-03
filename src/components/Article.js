2ea2858a5b1fc83bb3adb1bc82b07ecb9135e4f6
temptado <jason1287712@gmail.com>
2022-05-02 21:10:13 -0400

    created simple starter format for weather article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Article = () => {
     return (
-        <div>Day</div>
+        <article>
+            <h2>Date</h2>
+            <img src="" alt="" />
+            <p>temp</p>
+            <p>wind</p>
+            <p>humidity</p>
+
+        </article>
     )
 }
 
