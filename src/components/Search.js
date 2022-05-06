8d0453e7b402553379e3ee55a3c0bd1ea445f698
temptado <jason1287712@gmail.com>
2022-05-05 23:34:34 -0400

    added search button and created flex box

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,7 @@ const Search = () => {
         <div className='search-area'>
             <form action="submit" className='search-form'>
                 <input type="text" className='search' />
+                <button action='submit' className='submit-btn'>S</button>
             </form>
         </div>
     )
