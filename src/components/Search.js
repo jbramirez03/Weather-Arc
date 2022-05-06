c4ec0926ca75568f5528fab346a1f24f97b16765
temptado <jason1287712@gmail.com>
2022-05-05 23:56:54 -0400

    added search icon

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
+import { RiSearchLine } from 'react-icons/ri';
 
 const Search = () => {
     return (
         <div className='search-area'>
             <form action="submit" className='search-form'>
                 <input type="text" className='search' />
-                <button action='submit' className='submit-btn'>S</button>
+                <button action='submit' className='submit-btn'><RiSearchLine /></button>
             </form>
         </div>
     )
