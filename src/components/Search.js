5843b1ef17667d8515fb44f11aca2a88aad0c524
temptado <jason1287712@gmail.com>
2022-05-07 22:53:37 -0400

    allowed clear button to empty search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,14 @@ import { RiSearchLine } from 'react-icons/ri';
 
 const Search = () => {
     const [active, setActive] = useState(false);
+    const [currentSearch, setCurrentSearch] = useState('');
 
     const handleToggle = () => {
         setActive(!active);
+    }
+
+    const handleClear = () => {
+        setCurrentSearch('');
     }
 
     return (
@@ -13,8 +18,9 @@ const Search = () => {
             <div className={active ? 'search active' : 'search'}>
                 <div className='icon' onClick={handleToggle}></div>
                 <div className="input">
-                    <input type="text" placeholder='Search' id='city-search' />
+                    <input type="text" placeholder='Search' id='city-search' onChange={e => setCurrentSearch(e.target.value)} value={currentSearch} />
                 </div>
+                <span className="clear" onClick={handleClear}></span>
             </div>
         </div>
     )
