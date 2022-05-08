d1441b756e83dca5a6a0ad2385e183751371df9d
temptado <jason1287712@gmail.com>
2022-05-07 22:22:15 -0400

    added active toggle with react

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,16 @@ import { useState } from 'react';
 import { RiSearchLine } from 'react-icons/ri';
 
 const Search = () => {
+    const [active, setActive] = useState(false);
+
+    const handleToggle = () => {
+        setActive(!active);
+    }
+
     return (
         <div className='search-area'>
             <div className='search'>
-                <div className="icon"></div>
+                <div className="icon" onClick={handleToggle}></div>
                 <div className="input"></div>
             </div>
         </div>
