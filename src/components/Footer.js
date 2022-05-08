638841f471d2679bf96827d8951282d9703777c2
temptado <jason1287712@gmail.com>
2022-05-07 20:24:36 -0400

    moved footer to bottom of webpage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,15 @@ import { BsFillPersonFill } from 'react-icons/bs';
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="links">
-                <a href="https://github.com/jbramirez03"><RiGithubFill /></a>
-                <a href="https://www.linkedin.com/in/jason-barrera-ramirez-b2a473204/"><RiLinkedinBoxFill /></a>
-                <a href="https://jbramirez03.github.io/React-Portfolio/"><BsFillPersonFill /></a>
-            </div>
-            <div className="copyright">
-                <a className='repo-link' href="https://github.com/jbramirez03/Weather-Arc" target="_blank" rel='noreferrer'>&#169; jbramirez03. All rights reserved</a>
+            <div className='footer-content'>
+                <div className="links">
+                    <a href="https://github.com/jbramirez03"><RiGithubFill /></a>
+                    <a href="https://www.linkedin.com/in/jason-barrera-ramirez-b2a473204/"><RiLinkedinBoxFill /></a>
+                    <a href="https://jbramirez03.github.io/React-Portfolio/"><BsFillPersonFill /></a>
+                </div>
+                <div className="copyright">
+                    <a className='repo-link' href="https://github.com/jbramirez03/Weather-Arc" target="_blank" rel='noreferrer'>&#169; jbramirez03. All rights reserved</a>
+                </div>
             </div>
         </div>
     )
