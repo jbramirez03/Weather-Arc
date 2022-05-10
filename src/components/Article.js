e8fceb62dbda69ce2a499b151a8424bcbdbc3a92
temptado <jason1287712@gmail.com>
2022-05-09 21:24:54 -0400

    started function to revert all grid areas to origin when one is clicked

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-const Article = () => {
+const Article = ({ identifier }) => {
     const [active, setActive] = useState(false);
 
-    // const handleActive = () => {
-    //     if (active) {
-    //         return;
-    //     } else {
-    //         setActive(false);
-    //     }
-    // }
+    const handleNewClick = () => {
+        const filtered = [];
+        active ? setActive(false) : setActive(true);
+        const articles = document.querySelectorAll('.weather-article');
+        const currentArticle = articles[identifier];
+        for (let i = 0; i < articles.length; i++) {
+            if (articles[i] !== currentArticle) {
+                filtered.push(articles[i]);
+            }
+        }
+
+    }
+
 
     return (
-        <article className={active ? 'weather-article active' : 'weather-article'} onClick={() => { active ? setActive(false) : setActive(true) }}>
+        <article className={active ? 'weather-article active' : 'weather-article'} onClick={() => { handleNewClick() }}>
             <h2>Date</h2>
             <p>Temp:</p>
             <p>Wind:</p>
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,14 +3,14 @@ import Article from './Article';
 import CurrentDay from './CurrentDay';
 
 const Grid = () => {
-    const articles = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+    const articles = [{ key: 0, day: 'monday' }, { key: 1, day: 'tuesday' }, { key: 2, day: 'wednesday' }, { key: 3, day: 'thursday' }, { key: 4, day: 'friday' }];
 
 
     return (
         <div className='weather-grid'>
             <CurrentDay />
             {articles.map(article => (
-                <Article key={article} />
+                <Article key={article.day} identifier={article.key} />
             ))}
         </div>
     )
