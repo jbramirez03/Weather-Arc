8aadba8a9450a864e17428c5590835dbeae3b0d2
temptado <jason1287712@gmail.com>
2022-05-17 22:34:31 -0400

    modified current day weather article for mobile view

--- src/components/CurrentDay.js
+++ src/components/CurrentDay.js
@@ -3,11 +3,16 @@
 const CurrentDay = () => {
     return (
         <article className="currentDay">
-            <h1>Today's weather: </h1>
-            <h3>Date</h3>
-            <p>Temp:</p>
-            <p>Wind:</p>
-            <p>Humidity:</p>
+            <div className="currentDay-img">
+                <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" />
+            </div>
+            <div className="currentDay-details">
+                <h1>Today's weather: </h1>
+                <h3>Date</h3>
+                <p>Temp:</p>
+                <p>Wind:</p>
+                <p>Humidity:</p>
+            </div>
         </article>
     )
 }
