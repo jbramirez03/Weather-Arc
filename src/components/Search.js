0f99c45fb4b5465e87ee669f4d993e4349e47984
temptado <jason1287712@gmail.com>
2022-05-19 22:14:26 -0400

    wrapped input for search withing a form with submit event

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,9 +21,11 @@ const Search = () => {
         <div className='search-area'>
             <div className={active ? 'search active' : 'search'}>
                 <div className='icon' onClick={handleToggle}></div>
-                <div className="input">
-                    <input type="text" placeholder='Search' id='city-search' onChange={e => setCurrentSearch(e.target.value)} value={currentSearch} />
-                </div>
+                <form action="submit">
+                    <div className="input">
+                        <input type="text" placeholder='Search' id='city-search' onChange={e => setCurrentSearch(e.target.value)} value={currentSearch} />
+                    </div>
+                </form>
                 <span className="clear" onClick={handleClear}></span>
             </div>
         </div>
