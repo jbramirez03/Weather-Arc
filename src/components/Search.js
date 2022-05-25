17a4c717fdd08dd95208af9d95649cade6e623e1
temptado <jason1287712@gmail.com>
2022-05-24 20:49:46 -0400

    moved the search handler function into context in order to be used within search and current day component on render

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import moment from 'moment';
 
 const Search = () => {
-    const { citySearched, setToday } = useContext(WeatherContext);
+    const { searchHandler } = useContext(WeatherContext);
 
     const [active, setActive] = useState(false);
     const [currentSearch, setCurrentSearch] = useState('');
@@ -17,31 +17,9 @@ const Search = () => {
         setCurrentSearch('');
     }
 
-    const searchHandler = async () => {
-        const searchedCity = "https://api.openweathermap.org/data/2.5/weather?q=" + currentSearch + "&limit=1&appid=bcf6554b28b8c3bcc30e90eb27275f00"
-        try {
-            const returnedData = await axios.get(searchedCity);
-            const searchedWeekly = "https://api.openweathermap.org/data/2.5/onecall?lat=" + returnedData.data.coord.lat + "&lon=" + returnedData.data.coord.lon + "&units=imperial&appid=bcf6554b28b8c3bcc30e90eb27275f00";
-            const returnedWeeklyData = await axios.get(searchedWeekly);
-            const todayData = returnedWeeklyData.data.current;
-            setToday(
-                returnedData.data.name,
-                moment.unix(todayData.dt).format("MM/DD/YYYY"),
-                todayData.temp,
-                todayData.wind_speed,
-                todayData.humidity,
-                "https://openweathermap.org/img/wn/" + todayData.weather[0].icon + "@2x.png"
-            );
-            console.log(returnedData);
-            console.log(returnedWeeklyData);
-        } catch (error) {
-            throw new error
-        }
-    }
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        searchHandler();
+        searchHandler(currentSearch);
         setCurrentSearch('');
     }
 
--- src/utils/WeatherContext.js
+++ src/utils/WeatherContext.js
@@ -1,4 +1,6 @@
 import { createContext, useState } from 'react';
+import axios from 'axios';
+import moment from 'moment';
 
 const WeatherContext = createContext();
 
@@ -22,8 +24,30 @@ export function WeatherProvider(children) {
         });
     }
 
+    const searchHandler = async (currentSearch) => {
+        const searchedCity = "https://api.openweathermap.org/data/2.5/weather?q=" + currentSearch + "&limit=1&appid=bcf6554b28b8c3bcc30e90eb27275f00"
+        try {
+            const returnedData = await axios.get(searchedCity);
+            const searchedWeekly = "https://api.openweathermap.org/data/2.5/onecall?lat=" + returnedData.data.coord.lat + "&lon=" + returnedData.data.coord.lon + "&units=imperial&appid=bcf6554b28b8c3bcc30e90eb27275f00";
+            const returnedWeeklyData = await axios.get(searchedWeekly);
+            const todayData = returnedWeeklyData.data.current;
+            setToday(
+                returnedData.data.name,
+                moment.unix(todayData.dt).format("MM/DD/YYYY"),
+                todayData.temp,
+                todayData.wind_speed,
+                todayData.humidity,
+                "https://openweathermap.org/img/wn/" + todayData.weather[0].icon + "@2x.png"
+            );
+            console.log(returnedData);
+            console.log(returnedWeeklyData);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-    return <WeatherContext.Provider value={{ citySearched, setToday }} {...children} />;
+
+    return <WeatherContext.Provider value={{ citySearched, setToday, searchHandler }} {...children} />;
 }
 
 export default WeatherContext;
