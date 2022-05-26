ce724b73934da2722b71a6cc37ce22e3837151ca
temptado <jason1287712@gmail.com>
2022-05-25 22:16:54 -0400

    created conditional rendering for skeleton load on site visit

--- src/components/CurrentDay.js
+++ src/components/CurrentDay.js
@@ -4,36 +4,41 @@ import WeatherContext from "../utils/WeatherContext"
 const CurrentDay = () => {
     const { citySearched, searchHandler } = useContext(WeatherContext);
 
-    // useEffect(() => {
-    //     if (citySearched.name === '')
-    //         searchHandler('New York');
-    // }, [citySearched]);
+    useEffect(() => {
+        if (citySearched.name === '')
+            searchHandler('New York');
+    }, [citySearched]);
 
+
+    if (citySearched.name === '') {
+        return (
+            <article className="currentDay-skeleton">
+                <div className="currentImage-skeleton">
+                </div>
+                <div className="details-skeleton">
+                    <h1></h1>
+                    <h3></h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </article>
+        )
+    }
 
     return (
-        // <article className="currentDay">
-        //     <div className="currentImage">
-        //         <div className="currentDay-img">
-        //             <img src={citySearched.image} alt="weather icon" />
-        //         </div>
-        //     </div>
-        //     <div className="currentDay-details">
-        //         <h1>{citySearched.name}</h1>
-        //         <h3>{citySearched.date}</h3>
-        //         <p>Temp: {citySearched.temp}</p>
-        //         <p>Wind: {citySearched.wind}</p>
-        //         <p>Humidity: {citySearched.humidity}</p>
-        //     </div>
-        // </article>
-        <article className="currentDay-skeleton">
-            <div className="currentImage-skeleton">
+        <article className="currentDay">
+            <div className="currentImage">
+                <div className="currentDay-img">
+                    <img src={citySearched.image} alt="weather icon" />
+                </div>
             </div>
-            <div className="details-skeleton">
-                <h1></h1>
-                <h3></h3>
-                <p></p>
-                <p></p>
-                <p></p>
+            <div className="currentDay-details">
+                <h1>{citySearched.name}</h1>
+                <h3>{citySearched.date}</h3>
+                <p>Temp: {citySearched.temp}</p>
+                <p>Wind: {citySearched.wind}</p>
+                <p>Humidity: {citySearched.humidity}</p>
             </div>
         </article>
     )
